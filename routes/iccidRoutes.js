b74f08dcacec8eb9cd72a8a07042eca808259494
Alp Eren <erenalpturk@gmail.com>
2024-05-31 14:32:12 +0300

    post

--- routes/iccidRoutes.js
+++ routes/iccidRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const iccidController = require("../controllers/iccidController.js");
 
-router.get("/getIccid", iccidController.getIccid);
+router.post("/getIccid", iccidController.getIccid);
 router.post("/setSold", iccidController.setSold);
 router.post("/setAvailable", iccidController.setAvailable);
 router.post("/addIccid", iccidController.addIccid);
